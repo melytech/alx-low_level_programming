6f299d6c09820b400354ec17ecb1a2b94d061af1
melytech <chiclassic94@gmail.com>
2022-09-08 15:49:56 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - This program prints a specified message, followed by a new line
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
   printf("with proper grammar, but the outcome is a piece of art,\n");
-
   return (0);
 }
 
