87773d08cffd12ac78b2082ea0acd592d1eb1c2a
melytech <chiclassic94@gmail.com>
2022-09-08 14:03:06 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>                                                                                                                                    
+/**                                                                                                                                                   
+ * main - This program prints specif                                                                                                                  
+ed text using puts function                                                                                                                           
+* Return: Returns 0 on successful c                                                                                                                   
+ompletion                                                                                                                                             
+*/                                                                                                                                                    
+int main(void)                                                                                                                                        
+{                                                                                                                                                     
+  puts("\"Programming is lik                                                                                                                          
+e building a multilingual puzzle"                                                                                                                     
+       );                                                                                                                                             
+  return (0);                                                                                                                                         
+}                 
