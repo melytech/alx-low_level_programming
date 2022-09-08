9762c10ec000bae2b3f5546ea945170c3c6625e5
melytech <chiclassic94@gmail.com>
2022-09-08 14:48:16 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,17 @@
-#include <stdio.h>
-/**
-* main - This program prints "Programming is like building a multilingual puzzle" followed by a new line.
-* Return: Always 0 on successful completion
-*/
-int main(void)
-{
+#include <stdio.h>                                                                                                                      
+/**                                                                                                                                     
+* main - This program prints "Programming is like building a multilingual puzzle"                                                       
+*/                                                                                                                                      
+int main(void)                                                                                                                          
+{                                                                                                                                       
+root@a56669d72bbe:/alx-low_level_programming/0x00-hello_world# cat > 4-puts.c                                                           
+#include <stdio.h>                                                                                                                      
+/**                                                                                                                                     
+* main - This program prints "Programming is like building a multilingual puzzle"                                                       
+* Return: Always 0 (Success)                                                                                                            
+*/                                                                                                                                      
+int main(void)                                                                                                                          
+{    
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
