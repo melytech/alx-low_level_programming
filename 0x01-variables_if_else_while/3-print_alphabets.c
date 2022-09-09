b41afc3429c805f48efb5328fdeac69035f9d5b8
melytech <chiclassic94@gmail.com>
2022-09-09 16:27:56 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main - Write a program that prints the alphabet in lowercase, and then in uppercase
+* Return: Always true
+/*
+int main(void)
+{
+char alpha = 97;
+
+	while (alpha <= 122)
+	{
+		putchar(alpha++);
+	}
+
+	alpha = 65;
+
+	while (alpha <= 90)
+	{
+		putchar(alpha++);
+	}
+	putchar('\n');
+
+	return (0);
+}
