10c822f77b4e43d339dd41499faaec8d47501d6f
melytech <chiclassic94@gmail.com>
2022-09-08 18:31:45 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,13 @@
-#include <stdio.h>                                                                                                                   
-                                                                                                                                      
-/**                                                                                                                                   
- * main - Entry point                                                                                                                 
- *                                                                                                                                    
- * Return: Always 1 (success)                                                                                                         
- */                                                                                                                                   
-int main(void)                                                                                                                        
-{                                                                                                                                     
-  write(2,                                                                                                                            
-        "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",                                                              
-        59);
-  return (1);                                                                                                                         
-}                 
+#include <stdio.h>
 
+/**
+* main - Entry point
+* Return: Always 1 (success)
+*/
+int main(void)
+{
+	char str[] = "-Dora Korpar, 2015-10-19\n";
+
+	fprintf(stderr, "and that piece of art is useful\" %s", str);
+	return (1);
+}
