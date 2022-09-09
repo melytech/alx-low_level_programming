2765038ddfcf390f8cfcca5a942412fbaa76ace8
melytech <chiclassic94@gmail.com>
2022-09-09 16:54:14 -0700

    Write a program that prints all single digit numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-* main - Write a program that prints all single digit numbers of base 10 starting from 0
-* Return: Always true
+* main - Write a program that prints all single digit numbers 
+* Return: Returns always true
 */
 int main(void)
 {
-int n;
+	int n;
 
 	for (n = 0; n <= 9; n++)
 		printf("%i", n);
